a6e9c9d567c0730a6a63c0feb6f7f6ae86a9fe00
Vladislav Demyan <deellay.dev@gmail.com>
2022-02-15 01:28:14 +0200

    added pagination component

---
+++ homeWork-13/src/common/components/Pagination/Pagination.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from "react";
+import ReactPaginate from "react-paginate";
+import styled from "styled-components";
+import leftArrow from '../../../assests/icons/left_arrow.svg'
+import rightArrow from '../../../assests/icons/right-arrow.svg'
+
+
+
+export interface PaginationProps {
+  countPages: number;
+}
+
+export const Pagination: FC<PaginationProps> = ({countPages}) => {
+  const StyledPagination = styled(ReactPaginate)`
+  display: flex;
+  & > li {
+    margin: 0 5px;
+    width: 40px;
+    height: 40px;
+  }
+  & > li > a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 4px;
+    color:${({theme}) => theme.colors.grey};
+    cursor: pointer;
+    &:hover {
+      background-color: ${({theme}) => theme.colors.lightestGrey};
+    }
+  }
+  & > .active__page > a {
+    background-color: ${({theme}) => theme.colors.red};
+    color: ${({theme}) => theme.colors.white};
+    &:hover {
+      background-color: ${({theme}) => theme.colors.red};
+    }
+  }
+  & > .disabled > a {
+    cursor: default;
+    &:hover {
+      background-color: transparent;
+    }
+  }
+`
+  return (
+    <StyledPagination pageCount={countPages} activeClassName="active__page" previousLabel={<img src={leftArrow}/>} nextLabel={<img src={rightArrow}/>}
+    />
+  )
+};
